04e2cd201e271549d1c1d6fe33b39b6b8faf775b
siriusmart <siriusmart@tuta.io>
2025-06-21 14:58:49 +0100

    stuff

--- home/logo/pop.js
+++ home/logo/pop.js
@@ -1,13 +1,35 @@
 new Audio("/home/logo/swoosh.opus");
+new Audio("/home/logo/tap.opus");
 
-document.querySelectorAll("#topurls a").forEach((url) => {
-    url.onmouseover = () => {
-        let hoverSound = new Audio("/home/logo/hover.opus");
-        hoverSound.play();
-    };
+Array.from(document.querySelectorAll("#topurls a"))
+    .concat(Array.from(document.querySelectorAll("#socials a")).slice(1))
+    .forEach((url) => {
+        if (url.id == "emailbelowsocials") return;
+        if (!window.isMobile)
+            url.onmouseover = () => {
+                let hoverSound = new Audio("/home/logo/hover.opus");
+                hoverSound.play();
+            };
 
-    url.onclick = () => {
-        let hoverSound = new Audio("/home/logo/swoosh.opus");
+        url.onclick = () => {
+            let hoverSound = new Audio("/home/logo/swoosh.opus");
+            hoverSound.play();
+        };
+    });
+
+[
+    document.querySelector("#socials a"),
+    document.querySelector("#emailbelow a"),
+    document.getElementById("emailbelowsocials"),
+].forEach((email) => {
+    if (!window.isMobile)
+        email.onmouseover = () => {
+            let hoverSound = new Audio("/home/logo/hover.opus");
+            hoverSound.play();
+        };
+
+    email.onclick = () => {
+        let hoverSound = new Audio("/home/logo/tap.opus");
         hoverSound.play();
     };
 });
