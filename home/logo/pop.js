a63ae13b070046b7838914be2ac0e5ab71e30be0
siriusmart <siriusmart@tuta.io>
2025-06-29 18:43:10 +0100

    fixed hovers being incorrect

--- home/logo/pop.js
+++ home/logo/pop.js
@@ -1,42 +1,28 @@
 new Audio("/home/logo/swoosh.opus");
 new Audio("/home/logo/tap.opus");
 
-Array.from(document.querySelectorAll("#topurls a"))
-    .concat(Array.from(document.querySelectorAll("#socials a")).slice(1))
-    .concat(Array.from(document.querySelectorAll("#credit a")))
-    .forEach((url) => {
-        if (url.id == "emailbelowsocials") return;
-        if (!window.isMobile)
-            url.onmouseover = () => {
-                let hoverSound = new Audio("/home/logo/hover.opus");
-                hoverSound.play();
-            };
-
-        url.onclick = () => {
-            let hoverSound = new Audio("/home/logo/swoosh.opus");
-            hoverSound.play();
-        };
-    });
-
-[
-    document.querySelector("#socials a"),
-    document.querySelector("#emailbelow a"),
-    document.getElementById("emailbelowsocials"),
-].forEach((email) => {
+Array.from(document.querySelectorAll(".popurl")).forEach((url) => {
     if (!window.isMobile)
-        email.onmouseover = () => {
+        url.onmouseover = (e) => {
+            if (e.target != url) return;
             let hoverSound = new Audio("/home/logo/hover.opus");
             hoverSound.play();
         };
+});
 
-    email.onclick = () => {
+Array.from(document.querySelectorAll(".whooshurl")).forEach((url) => {
+    url.onclick = (e) => {
+        if (e.target != url) return;
+        let hoverSound = new Audio("/home/logo/swoosh.opus");
+        hoverSound.play();
+    };
+});
+
+Array.from(document.querySelectorAll(".takurl")).forEach((url) => {
+    url.onclick = (e) => {
+        if (e.target != url) return;
         let hoverSound = new Audio("/home/logo/tap.opus");
         hoverSound.play();
-
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
     };
 });
 
